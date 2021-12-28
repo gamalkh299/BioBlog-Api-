2b747d1b047ccb6a666b06f527a2537cf8ef1143
gamal khaled <jamal2014.1440@gmail.com>
2021-12-28 17:26:20 +0200

    api unversioned files #1

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    use ApiResponse;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Post::published()->paginate(15);
+        PostResource::collection($data);
+        return $this->ApiResponse($data);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post=Post::find($id);
+
+        if ($post){
+            return $this->ApiResponse(new PostResource($post));
+
+        }else{
+            return $this->ApiResponse(null,'data Not Found' , 404);
+        }
+    }
+
+    public function GetLatestPosts()
+    {
+        $data = Post::latest()->take(15)->paginate();
+        PostResource::collection($data);
+        return $this->ApiResponse($data);
+
+    }
+
+
+
+}
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\AbstractPaginator;
+use PhpParser\Node\Expr\New_;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+                    'id'=> $this->id,
+                    'title'=>$this->title,
+                    'description'=>$this->description,
+                    'image'=> $this->image->url(),
+                    'tags'=>$this->tags,
+                    'categories'=>$this->categories,
+
+        ];
+    }
+}
