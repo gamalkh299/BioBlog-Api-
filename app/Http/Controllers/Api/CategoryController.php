a989da979ed4f71ec4c8170cb26dec53f8ba6c5e
gamal khaled <jamal2014.1440@gmail.com>
2021-12-30 01:43:59 +0200

    edit posts of category Api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\PostResource;
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -32,11 +33,10 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        $cat=Category::with('posts')->where('id',$id)->get();
-//        dd($cat);
-        if ($cat){
-            $data=CategoryResource::collection($cat);
-            return $this->ApiResponse($data);
+        $posts=Post::query()->whereRelation('categories','categories.id','=',$id)->paginate(15);
+        if ($posts){
+            PostResource::collection($posts);
+            return $this->ApiResponse($posts);
 
         }else{
             return $this->ApiResponse(null,'data Not Found' , 404);
